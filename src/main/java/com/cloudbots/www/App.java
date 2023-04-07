9da91037301625d454b281acfaf284eff8da9f5d
venkatesh-5 <128568838+venkatesh-5@users.noreply.github.com>
2023-04-07 12:29:35 +0530

    Update App.java

--- src/main/java/com/cloudbots/www/App.java
+++ src/main/java/com/cloudbots/www/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! __version__ - 2.0" );
+        System.out.println( "Hello World! __version__ - 3.0" );
     }
 }
